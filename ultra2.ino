9e6c71c32e9657bc489cf2d7834d04947c8da052
chlalszxc <162539461+chlalszxc@users.noreply.github.com>
2024-03-18 22:38:41 +0900

    Add files via upload

---
+++ ultra2.ino
@@ -0,0 +1,24 @@
+#include <NewPing.h>
+
+
+
+#define TRIGGER_PIN 12
+#define ECHO_PIN 11
+#define MAX_DISTANCE 100
+//sonar(TrigPin, EchoPin, MaxDistance);
+
+NewPing sonar(TRIGGER_PIN,ECHO_PIN, MAX_DISTANCE);
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  float distance = 0.0;
+  delay(50);
+  distance = sonar.ping_cm();
+  if(distance == 0) distance = MAX_DISTANCE;         
+  Serial.print("Ping : ");
+  Serial.print(distance);
+  Serial.println("cm");
+}
